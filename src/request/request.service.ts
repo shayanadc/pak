8f5e3ead419e5d949c028b6319d966b629a0d9d0
shayan <shayanadc@gmail.com>
2021-01-17 15:50:06 +0330

    fix period

--- src/request/request.service.ts
+++ src/request/request.service.ts
@@ -24,7 +24,6 @@ export class RequestService {
     newReq.date = nextDate;
     newReq.done = false;
     newReq = await this.requestRepo.save(newReq);
-    console.log(newReq.address);
     return newReq;
   }
   calcNextTime(date, period) {
